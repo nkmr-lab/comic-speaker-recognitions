523ae2e0f6a57e8c407e76b1ddf47116da45a5b6
i3ear <ai3ekzk@gmail.com>
2020-01-22 21:08:25 +0900

    :sparkles: add calc_inframe.py
    フレーム内にあるキャラクタをもとに得点するプログラムを作成

--- settings.py
+++ settings.py
@@ -1,6 +1,17 @@
 
+import os
+import manga109api
+from dotenv import load_dotenv
+
+# manga109の設定
+load_dotenv()
+MANGA109_ROOT_DIR = os.environ['MANGA109_ROOT_DIR']
+manga109_parser = manga109api.Parser(root_dir=MANGA109_ROOT_DIR)
+
+# データセットの設定
 DATASET_NAME = 'all'
 
+# 計算方法の設定
 SCORE_TYPES = [
     'neighbor'
 ]

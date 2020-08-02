29879f2c958f1eab267e830e75f167662c1af63d
i3ear <ai3ekzk@gmail.com>
2020-08-02 15:26:00 +0900

    Add: Makefileで引数とともに簡単に動かせる仕様に変更

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 import manga109api
 from dotenv import load_dotenv
 
@@ -13,6 +14,8 @@ MECAB_IPADIC_PATH = os.environ['MECAB_IPADIC_PATH']
 
 # データセットの設定
 DATASET_NAME = 'top5'
+if len(sys.argv) > 1:
+    DATASET_NAME = sys.argv[1]
 
 # 計算方法の設定
 SCORE_TYPES = [
@@ -21,5 +24,7 @@ SCORE_TYPES = [
     'taildirection3',
     'firstperson', 'endingword'
 ]
+if len(sys.argv) > 2:
+    SCORE_TYPES = sys.argv[2].split(',')
 
 UPDATE = True
